4441c5551b2ccd4a22afa9049d464a4c28055e3e
danyr59 <danyelrange59@gmail.com>
2021-06-25 07:28:13 -0400

    add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,121 @@
-import logo from "./image/logo.svg";
+import "./style/App.scss";
+import React, { Component } from "react";
+import { bankOne, bankTwo } from "./services/Banks.js";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      power: true,
+      sliderVal: 50,
+      display: String.fromCharCode(160),
+      currentPadBank: bankOne,
+      currentPadBankId: "Heater Kit",
+    };
+    this.handlerSelectPower = this.handlerSelectPower.bind(this);
+    this.adjustVolume = this.adjustVolume.bind(this);
+    this.selectBank = this.selectBank.bind(this);
+  }
+  selectBank() {
+    if (this.state.power) {
+      if (this.state.currentPadBankId === "Heater Kit") {
+        this.setState({
+          currentPadBank: bankTwo,
+          display: "Smooth Piano Kit",
+          currentPadBankId: "Smooth Piano Kit",
+        });
+      } else {
+        this.setState({
+          currentPadBank: bankOne,
+          display: "Heater Kit",
+          currentPadBankId: "Heater Kit",
+        });
+      }
+    }
+  }
+  handlerSelectPower() {
+    this.setState((state) => ({
+      power: !state.power,
+      display: String.fromCharCode(160),
+    }));
+  }
+  adjustVolume(e) {
+    this.setState((state) => ({
+      sliderVal: e.target.value,
+      display: "Volume" + " " + Math.round(e.target.value),
+    }));
+    setTimeout(() => this.clearDisplay(), 1000);
+  }
+  clearDisplay() {
+    this.setState({
+      display: String.fromCharCode(160),
+    });
+  }
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate()");
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header" id="drum-machine">
+          <div className="contenedor border border-warning border-5">
+            <div className="logo">
+              <i className="bi bi-music-note-beamed"></i>
+            </div>
+            <div className="contenedor2">
+              {/*
+              <PadBank />
+              */}
+              <div className="controls-container">
+                <div className="control">
+                  <p>Power</p>
+                  <div className="select" onClick={this.handlerSelectPower}>
+                    <div
+                      className="inner"
+                      style={
+                        this.state.power
+                          ? { float: "right" }
+                          : { float: "left" }
+                      }
+                    ></div>
+                  </div>
+                </div>
+                <p id="display">{this.state.display}</p>
+                <div>
+                  <input
+                    type="range"
+                    value={this.state.sliderVal}
+                    min="0"
+                    max="1 "
+                    step="0.01"
+                    onChange={this.adjustVolume}
+                  />
+                </div>
+                <div className="control">
+                  <p>Bank</p>
+                  <div className="select" onClick={this.selectBank}>
+                    <div
+                      className="inner"
+                      style={
+                        this.state.currentPadBankId === "Heater Kit"
+                          ? { float: "left" }
+                          : { float: "right" }
+                      }
+                    ></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
+/*import logo from "./image/logo.svg";
 import "./style/App.scss";
 
 function App() {
@@ -22,4 +139,4 @@ function App() {
   );
 }
 
-export default App;
+*/
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style/index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
